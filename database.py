014ee260dc26352330de88e74e4c2a4c3562a389
Facundo Büchert <facundobuchert@gmail.com>
2024-09-11 01:18:18 -0300

    Actualización

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# URL de la base de datos PostgreSQL obtenida de ElephantSQL, Supabase o cualquier otro servicio
-DATABASE_URL = "postgresql://postgres:UDmnXVvrQvxabWCTQzSzoHKTIwvaGDVM@meticulous-empathy.railway.internal:5432/railway"
+# URL de la base de datos PostgreSQL obtenida de una variable de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")  # La URL debe ser definida como variable de entorno en el servidor.
 
 # Crear el motor para SQLAlchemy
 engine = create_engine(DATABASE_URL)
@@ -13,6 +14,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base para declarar los modelos
 Base = declarative_base()
+
+# Crear las tablas en la base de datos
+Base.metadata.create_all(bind=engine)
 
 # Dependencia para obtener la sesión de la base de datos
 def get_db():
