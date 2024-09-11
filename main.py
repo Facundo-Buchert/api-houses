777b8264d76f27f496be2f714e5aecfb03940e87
Facundo Büchert <facundobuchert@gmail.com>
2024-09-11 01:35:28 -0300

    Actualización

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Habilitar CORS para permitir solicitudes desde el frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
